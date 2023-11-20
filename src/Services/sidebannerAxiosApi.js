31950ab6deaecb072f47692d8970500b12e80a58
jmraffi84 <jmraffi@gmail.com>
2023-11-20 15:57:47 +0530

    update url

--- src/Services/sidebannerAxiosApi.js
+++ src/Services/sidebannerAxiosApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = `http://localhost:3001`;
+const BASE_URL = `https://cap-be-0mzm.onrender.com/`;
 
 export const getAllSideImages = async () => {
     try {
